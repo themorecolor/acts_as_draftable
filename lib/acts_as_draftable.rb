36bc84838090942faf604631cd07409244e9a950
zl <zl361@dfc.cn>
2014-10-30 15:19:42 +0800

    ii

--- lib/acts_as_draftable.rb
+++ lib/acts_as_draftable.rb
@@ -1,7 +1,12 @@
+require 'active_record'
+require 'active_support/inflector'
 require "acts_as_draftable/version"
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 
 module ActsAsDraftable
   # Your code goes here...
+
   require 'acts_as_draftable/extenders/extend'
   require 'acts_as_draftable/draft'
 
