e236f5c6ac6e2f5058fabdabee798a7ca39e151e
zl <zl361@dfc.cn>
2014-10-30 18:42:15 +0800

    ii

--- lib/acts_as_draftable.rb
+++ lib/acts_as_draftable.rb
@@ -6,7 +6,6 @@ module ActsAsDraftable
   # Your code goes here...
 
   require 'acts_as_draftable/extenders/extend'
-  require 'acts_as_draftable/draft'
 
   ActiveRecord::Base.extend ActsAsDraftable::Extenders::Extend::Ownerable
   ActiveRecord::Base.extend ActsAsDraftable::Extenders::Extend::Draftable
--- lib/acts_as_draftable/version.rb
+++ lib/acts_as_draftable/version.rb
@@ -1,3 +1,3 @@
 module ActsAsDraftable
-  VERSION = "0.1.8"
+  VERSION = "0.1.9"
 end
