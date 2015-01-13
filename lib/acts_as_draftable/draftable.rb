9e4c0daaf59bca0e9f7d27cda876a1824bf2a39a
zl <zl361@dfc.cn>
2015-01-13 16:29:00 +0800

    update

--- lib/acts_as_draftable/draftable.rb
+++ lib/acts_as_draftable/draftable.rb
@@ -101,7 +101,7 @@ module ActsAsDraftable
       end
 
       def with_draft
-        if self.last_active_draft.present?
+        if self.last_draft.present? and [0, -1].include? self.last_draft.verified
           self.assign_attributes(self.last_draft.content_as_json)
         end
         self
